ef0cd43dd10f25f52042370ee3477ba458591206
marcevonku <marcevonku@gmail.com>
2024-02-15 16:50:47 -0300

    agregando barra nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import BackGround from "./Components/Bg.jsx";
+import NavBasic from "./Components/NavBasic.jsx"
 
 function App() {
 
   return (
     <>
-    <BackGround/>
+      <NavBasic/>
+      <BackGround/>
   </>
   );
 }
