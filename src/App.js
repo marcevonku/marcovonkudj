c25aa10bb1a6245604fd22d41154015b79723b4e
marcevonku <marcevonku@gmail.com>
2024-02-15 16:42:33 -0300

    tenia mal la dirección de pages del repo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
-import NavBasic from "./Components/NavBasic.jsx";
 import BackGround from "./Components/Bg.jsx";
-import BackGroundUno from "./Components/BgUno.jsx";
-import BackGroundDos from "./Components/BgDos.jsx";
-
 
 function App() {
 
   return (
     <>
-      <NavBasic />
-      <BackGround />
-      <BackGroundUno />
-      <BackGroundDos />
+    <BackGround/>
   </>
   );
 }
