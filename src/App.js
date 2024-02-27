f286db65eec91a0e8571e1839b89000fec40d992
marcevonku <marcevonku@gmail.com>
2024-02-27 18:13:36 -0300

    cambio de barra de menú

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BackGround from "./Components/Bg.jsx";
-import NavBasic from "./Components/NavBasic.jsx"
+// import NavBasic from "./Components/NavBasic.jsx"
 import Separador from "./Components/Separador.jsx";
 import MuroUno from './img-fondo/MuroUno.jpg';
 import marco_outfitUno from './img-fondo/marco_outfitUno.jpg';
@@ -10,12 +10,13 @@ import imgfiesta2 from './img-fondo/imgfiesta2.jpg';
 import marco_parqueUno from './img-fondo/marco_parqueUno.jpg';
 import Footer from "./Components/Footer.jsx";
 import Carrusel from "./Components/Carrusel";
+import NavBasic1 from "./Components/NavCasic1.jsx";
 
 function App() {
 
   return (
     <>
-      <NavBasic />
+      <NavBasic1 />
       <div className="clearfix" id="inicio"></div>
       <BackGround 
         fondo={MuroUno}//Aqui indico que imagen cargar en el background
@@ -27,7 +28,7 @@ function App() {
         
       />
       <div className="clearfix"></div>
-      <Separador title="Servicio: BAR && MUSIC HOUSE" id="eventos"/>
+      <Separador title="Servicio: BAR && MUSIC HOUSE" id="resto/3.1"/>
       <div className="clearfix"></div>
       <BackGround
         fondo={imgfiesta2}
@@ -38,25 +39,25 @@ function App() {
         val={2}
       />
       <div className="clearfix"></div>
-      <Separador title="Servicio: DISCO && DRINKS" id=""/>
+      <Separador title="Servicio: DISCO && DRINKS" id="bares/3.2"/>
       <div className="clearfix"></div>
       <BackGround 
         fondo={marco_outfitUno}
         val={3} />
       <div className="clearfix"></div>
-      <Separador title="Servicio: DISC ALL NIGHT" id="" />
+      <Separador title="Servicio: DISC ALL NIGHT" id="boliches/3.3" />
       <div className="clearfix"></div>
       <BackGround 
         fondo={marco_parqueUno}
         val={4} />
-      <Separador title="GALERIA DE FOTOS" id="galeria" />
+      <Separador title="GALERIA DE FOTOS" id="galeria/3.4" />
       <div className="clearfix"></div>
       <Carrusel />
-      <Separador title="EMPRESAS QUE ENTIENDEN" id="empresas" />
+      <Separador title="EMPRESAS QUE ENTIENDEN" id="empresas/3.5" />
       <div className="clearfix"></div>
       <BackGround fondo={"empresas"} />
       <div className="clearfix"></div>
-      <Separador title="  Quieres llevar tu fiesta al próximo nivel...!!!  ***  Envíame un mensaje" id="contacto"/>
+      <Separador title="  Quieres llevar tu fiesta al próximo nivel...!!!  ***  Envíame un mensaje" id="contacto/3.6"/>
       <div className="clearfix"></div>
       <BackGround fondo={"color"} />
       <Footer />
